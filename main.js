0de7cac9c2f3077efa91e7069b6c8e52bc841261
HuDaHei <1405544590@qq.com>
2019-03-30 22:34:40 +0800

    node 连接数据库

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
 const http = require('http');
-const url = require('url');
 const childProcess = require('child_process');
 const fs = require('fs');
-const path = require('path');
-
-
+const conn = require('./sql/server.js').connection;
+console.log(conn,"conn");
 const hostname = 'localhost';
 const port = 3000;
 // 创建服务器
-const server = http.createServer ( function(request, response){
-    // 读取test文件
-    fs.readFileSync('./webstatic/test.html', (err, data)=>{
-        if(err) {
-            response.statusCode = 400;
-            response.end('not Found')
-        } else {
-             
-        }
-    })
+const server = http.createServer(function (request, response) {
+	// 封装好的返回对象
+	let backData = {
+		code: '200',
+		data: {},
+		page: '1',
+		total: 400,
+	}
+	// 重定向 如果地址是 / 或者 /test.html 定向到文件的位置
+	let url = request.url;
+  if(url === '/' || url === '/test.html') {
+		fs.readFile('./webstatic/test.html', (err,data) => {
+			let end = '';
+			if(err) {
+				response.statusCode = 400;
+				end = 'NOT FOUND';
+			} else {
+				response.write(data);
+			}
+			response.end(end);
+		})
+	}
+	// 登陆
+	if(url === '/login') {
+		request.on('data', (buffer)=>{
+      console.log(buffer)
+		});
+		request.on('end',()=>{
+			let sql = 'select * from userInfo';
+			conn.query(sql,(error,result,fields)=>{
+				// if(error) throw error;
+				console.log(result[0],'result');
+				response.statusCode = 200;
+				response.write(JSON.stringify(result[0]));
+				response.end();
+			})
+		})
+	}
 });
-server.listen( port, hostname, ()=> {
-    console.log(`server run at http://${hostname}:${port}`);
-    childProcess.exec(`start http://${hostname}:${port}`);
+server.listen(port, hostname, () => {
+	console.log(`server run at http://${hostname}:${port}`);
+	childProcess.exec(`start http://${hostname}:${port}`);
 });
--- sql/server.js
+++ sql/server.js
@@ -1,4 +1,13 @@
-
+const mysql = require('mysql');
+let sqlConfig = {
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: '1234',
+  database: 'learndb'
+};
+// 连接数据库
+const connection = mysql.createConnection(sqlConfig)
 // 连接数据库
 module.exports = {
   connection
